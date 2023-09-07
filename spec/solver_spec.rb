89251cd871f03d853397ecbda005881477c884e1
rbd3 <rabedesanaa@gmail.com>
2023-09-07 20:13:47 +0300

    Add test for future Fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,53 +1,46 @@
-require_relative 'solver'
+require './solver'
 require 'rspec'
 
 describe Solver do
+  before(:each) do
+    @solver_instance = Solver.new
+  end
+
   describe '#factorial' do
     it 'returns the factorial of 3' do
-      solver = Solver.new
-      expect(solver.factorial(3)).to eq(6)
+      expect(@solver_instance.factorial(3)).to eq(6)
     end
 
     it 'returns the factorial of 0' do
-      solver = Solver.new
-      expect(solver.factorial(0)).to eq(1)
+      expect(@solver_instance.factorial(0)).to eq(1)
     end
 
     it 'raises an exception for negative numbers' do
-      solver = Solver.new
-      expect { solver.factorial(-5) }.to raise_error(ArgumentError)
+      expect { @solver_instance.factorial(-5) }.to raise_error(ArgumentError)
     end
   end
 
   describe '#reverse' do
-    it ' return a reversed string' do
-        string = Solver.new
-      expect(string.reverse('fitia')).to eq('aitif')
+    it 'returns a reversed string' do
+      expect(@solver_instance.reverse('fitia')).to eq('aitif')
     end
   end
 
   describe '#fizzbuzz' do
-    it ' return fizz because divisible by 3' do
-        number = Solver.new
-      expect(number.fizzbuzz(6)).to eq('fizz')
+    it 'returns "fizz" because divisible by 3' do
+      expect(@solver_instance.fizzbuzz(6)).to eq('fizz')
     end
 
-    it ' return buzz because divisible by 5' do
-        number = Solver.new
-      expect(number.fizzbuzz(10)).to eq('buzz')
+    it 'returns "buzz" because divisible by 5' do
+      expect(@solver_instance.fizzbuzz(10)).to eq('buzz')
     end
 
-    it ' return fizzbuzz because divisible by 3 and 5' do
-        number = Solver.new
-      expect(number.fizzbuzz(15)).to eq('fizzbuzz')
+    it 'returns "fizzbuzz" because divisible by 3 and 5' do
+      expect(@solver_instance.fizzbuzz(15)).to eq('fizzbuzz')
     end
 
-    it ' return number because not divisible by 3 or 5' do
-        number = Solver.new
-      expect(number.fizzbuzz(15)).to eq('fizzbuzz')
+    it 'returns number as a string because not divisible by 3 or 5' do
+      expect(@solver_instance.fizzbuzz(7)).to eq('7')
     end
-
   end
-  
-
 end
